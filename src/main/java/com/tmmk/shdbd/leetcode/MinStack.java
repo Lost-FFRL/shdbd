28b4c721398c9d17abb1f581900bba44b24a1c6b
Lost <308060655@qq.com>
2020-04-17 10:22:57 +0800

    <add> 生命算法

---
+++ src/main/java/com/tmmk/shdbd/leetcode/MinStack.java
@@ -0,0 +1,75 @@
+package com.tmmk.shdbd.leetcode;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * push(x) -- 将元素 x 推入栈中。
+ * pop() -- 删除栈顶的元素。
+ * top() -- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ * 示例:
+ * <p>
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-stack
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Lost
+ * @date 2020-03-25 15:16
+ * @see
+ * @since 1.9.0
+ */
+public class MinStack {
+
+
+
+    /** initialize your data structure here. */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+
+
+    }
+
+    // 移除
+    public void pop() {
+
+    }
+
+    // 返回
+    public int top() {
+        return 0;
+    }
+
+    //
+    public int getMin() {
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}
