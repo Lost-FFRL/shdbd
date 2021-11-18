0b2facc418b4797d4eb1abbfbf8918c46853407e
Lost <308060655@qq.com>
2021-11-18 18:16:32 +0800

    feat: contains-duplicate

---
+++ src/main/java/com/tmmk/shdbd/leetcode/ContainsDuplicate.java
@@ -0,0 +1,60 @@
+package com.tmmk.shdbd.leetcode;
+
+/**
+ * 给定一个整数数组，判断是否存在重复元素。
+ *
+ * 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ * 示例 1:
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 示例 2:
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 示例 3:
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author Lost
+ * @version 1.0.0
+ * @date 2021-11-18 18:07
+ */
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        if (null != nums && nums.length > 1) {
+            int c;
+            int size = nums.length;
+            for (int i = 1; i < size; i++) {
+                if (nums[i] == nums[i - 1]) {
+                    return  true;
+                } else if (nums[i] < nums[i - 1]) {
+                    c = nums[i];
+                    nums[i] = nums[i - 1];
+                    nums[i - 1] = c;
+                    for (int j = i - 1; j > 0; j--) {
+                        if (nums[j] == nums[j - 1]) {
+                            return true;
+                        } else if (nums[j] < nums[j - 1]) {
+                            c = nums[j];
+                            nums[j] = nums[j - 1];
+                            nums[j - 1] = c;
+                        }else{
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
